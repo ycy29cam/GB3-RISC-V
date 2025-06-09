91c8eddc6ead993c88f0062981cda88a8042309c
Phil <ycy29@cam.ac.uk>
2025-06-09 19:15:02 +0100

    Program counter changes

--- processor/sail-core/verilog/program_counter.v
+++ processor/sail-core/verilog/program_counter.v
@@ -1,68 +1,23 @@
-/*
-	Authored 2018-2019, Ryan Voo.
+// program_counter.v  (Option A, no rst, no stall)
+module program_counter #(
+    parameter RESET_VAL = 32'b0
+)(
+    input  logic        clk,
+    input  logic        branch_i,         // new: take branch_target_i
+    input  logic [31:0] branch_target_i,  // from addr_adder
+    output logic [31:0] pc_o              // replaces outAddr
+);
 
-	All rights reserved.
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions
-	are met:
+    // next‐PC mux
+    logic [31:0] pc_next;
+    always_comb begin
+        if (branch_i)      pc_next = branch_target_i;  
+        else                pc_next = pc_o + 32'd4;      // sequential +4
+    end
 
-	*	Redistributions of source code must retain the above
-		copyright notice, this list of conditions and the following
-		disclaimer.
-
-	*	Redistributions in binary form must reproduce the above
-		copyright notice, this list of conditions and the following
-		disclaimer in the documentation and/or other materials
-		provided with the distribution.
-
-	*	Neither the name of the author nor the names of its
-		contributors may be used to endorse or promote products
-		derived from this software without specific prior written
-		permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-	BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-	LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
-*/
-
-
-
-/*
- *	Description:
- *
- *		This module implements the program counter
- */
-
-
-
-module program_counter(inAddr, outAddr, clk);
-	input			clk;
-	input [31:0]		inAddr;
-	output reg[31:0]	outAddr;
-
-	/*
-	 *	This uses Yosys's support for nonzero initial values:
-	 *
-	 *		https://github.com/YosysHQ/yosys/commit/0793f1b196df536975a044a4ce53025c81d00c7f
-	 *
-	 *	Rather than using this simulation construct (`initial`),
-	 *	the design should instead use a reset signal going to
-	 *	modules in the design.
-	 */
-	initial begin
-		outAddr = 32'b0;
-	end
-
-	always @(posedge clk) begin
-		outAddr <= inAddr;
-	end
+    // PC register, no external reset/stall
+    initial pc_o = RESET_VAL;
+    always @(posedge clk) begin
+        pc_o <= pc_next;
+    end
 endmodule
