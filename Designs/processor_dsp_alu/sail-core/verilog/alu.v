0f9c83caad5ae2b4a31f60c18ef8725c4d09fcad
Phil <ycy29@cam.ac.uk>
2025-06-12 04:11:13 +0100

    Binary files generated, build messages saved

---
+++ Designs/processor_dsp_alu/sail-core/verilog/alu.v
@@ -0,0 +1,109 @@
+`include "../include/rv32i-defines.v"
+`include "../include/sail-core-defines.v"
+
+module alu(ALUctl, A, B, ALUOut, Branch_Enable);
+	input [6:0]		ALUctl;
+	input [31:0]		A;
+	input [31:0]		B;
+	output reg [31:0]	ALUOut;
+	output reg		Branch_Enable;
+
+	wire [31:0] adder_out;
+	wire subtraction;
+
+	initial begin
+		ALUOut = 32'b0;
+		Branch_Enable = 1'b0;
+	end
+
+	assign subtraction = (
+        ALUctl[3:0] == `kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SUB
+    );
+
+	adder_dsp alu_adder(
+		.input1(A),
+		.input2(B),
+		.is_sub(subtraction),
+		.out(adder_out),
+	);
+
+	always @(ALUctl, A, B) begin
+		case (ALUctl[3:0])
+			/*
+			 *	AND (the fields also match ANDI and LUI)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_AND:	ALUOut = A & B;
+
+			/*
+			 *	OR (the fields also match ORI)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_OR:	ALUOut = A | B;
+
+			/*
+			 *	ADD (the fields also match AUIPC, all loads, all stores, and ADDI)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_ADD:	ALUOut = adder_out;
+
+			/*
+			 *	SUBTRACT (the fields also matches all branches)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SUB:	ALUOut = adder_out;
+			/*
+			 *	SLT (the fields also matches all the other SLT variants)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SLT:	ALUOut = $signed(A) < $signed(B) ? 32'b1 : 32'b0;
+			/*
+			 *	SRL (the fields also matches the other SRL variants)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SRL:	ALUOut = A >> B[4:0];
+
+			/*
+			 *	SRA (the fields also matches the other SRA variants)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SRA:	ALUOut = $signed(A) >>> B[4:0];
+
+			/*
+			 *	SLL (the fields also match the other SLL variants)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SLL:	ALUOut = A << B[4:0];
+
+			/*
+			 *	XOR (the fields also match other XOR variants)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_XOR:	ALUOut = A ^ B;
+
+			/*
+			 *	CSRRW  only
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_CSRRW:	ALUOut = A;
+
+			/*
+			 *	CSRRS only
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_CSRRS:	ALUOut = A | B;
+
+			/*
+			 *	CSRRC only
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_CSRRC:	ALUOut = (~A) & B;
+
+			/*
+			 *	Should never happen.
+			 */
+			default:					ALUOut = 0;
+		endcase
+	end
+
+	always @(ALUctl, ALUOut, A, B) begin
+		case (ALUctl[6:4])
+			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BEQ:	Branch_Enable = (ALUOut == 0);
+			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BNE:	Branch_Enable = !(ALUOut == 0);
+			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BLT:	Branch_Enable = ($signed(A) < $signed(B));
+			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BGE:	Branch_Enable = ($signed(A) >= $signed(B));
+			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BLTU:	Branch_Enable = ($unsigned(A) < $unsigned(B));
+			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BGEU:	Branch_Enable = ($unsigned(A) >= $unsigned(B));
+
+			default:					Branch_Enable = 1'b0;
+		endcase
+	end
+endmodule
